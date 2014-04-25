176aad994ae2ddf0603b572ae57b88bfbed5708a
Hiltjo Posthuma <hiltjo@codemadness.org>
2014-04-25 22:22:18 +0200

    compile with -Wextra for now
    Signed-off-by: Hiltjo Posthuma <hiltjo@codemadness.org>

--- config.mk
+++ config.mk
@@ -9,5 +9,5 @@ MANPREFIX = $(PREFIX)/share/man
 #CC       = musl-gcc
 LD        = $(CC)
 CPPFLAGS  = -D_BSD_SOURCE
-CFLAGS    = -g -Wall -ansi $(CPPFLAGS)
+CFLAGS    = -g -Wall -Wextra -O0 -ansi $(CPPFLAGS)
 LDFLAGS   = -g
